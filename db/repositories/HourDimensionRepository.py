d0bd22056973e4121759828d2433722678f43438
Ashish Khiani <akhia013@uottawa.ca>
2019-03-04 13:41:27 -0500

    created HourDimensionRepository
    Class that interacts with the database

---
+++ db/repositories/HourDimensionRepository.py
@@ -0,0 +1,60 @@
+from db.DatabaseConnection import DatabaseConnection
+
+
+class HourDimensionRepository(object):
+    """
+    This functionality of this class is to interact with the database.
+    ALl methods defined in the class must be solely responsible for reading and writing to the database.
+    No business logic is allowed here.
+    """
+
+    @staticmethod
+    def insert(entity):
+        """
+        Inserts a single entity to the database.
+        :param entity: a tuple of the form ->
+                (hour_start, hour_end, date,day_of_week, month,year, is_weekend, is_holiday, holiday_name)
+        :return: None
+        """
+        db = DatabaseConnection()
+
+        sql_insert = """INSERT INTO accidents_weather_data_mart.hour_dimension (
+                            hour_start, 
+                            hour_end, 
+                            date, 
+                            day_of_week, 
+                            month, 
+                            year, 
+                            is_weekend, 
+                            is_holiday, 
+                            holiday_name) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+        with db.get_connection().cursor() as cursor:
+            cursor.execute(sql_insert, entity)
+
+    @staticmethod
+    def insert_many(entities):
+        """
+        Inserts a many entities at once to the database.
+        :param entities: a list of tuples of the form ->
+                [(hour_start, hour_end, date,day_of_week, month,year, is_weekend, is_holiday, holiday_name)]
+        :return: None
+        """
+        db = DatabaseConnection()
+
+        sql_insert = """INSERT INTO accidents_weather_data_mart.hour_dimension (
+                            hour_start, 
+                            hour_end, 
+                            date, 
+                            day_of_week, 
+                            month, 
+                            year, 
+                            is_weekend, 
+                            is_holiday, 
+                            holiday_name) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+        with db.get_connection().cursor() as cursor:
+            cursor.executemany(sql_insert, entities)
+
